5802062002e8297cdb8fb4e3aba10c5648ed0a41
Felipe Jiménez <anjimenezp@unal.edu.co>
2020-05-20 20:55:53 -0500

    jar

---
+++ src/Frontera/MainFront.java
@@ -0,0 +1,21 @@
+//+------------------------------------------------------------------+
+//|                                                   MainFront.java |
+//|                    2020-1S, COVID-19 Estadísticas Software Corp. |
+//|                            Andrés Jiménez anjimenezp@unal.edu.co |
+//|            Jhon Sebastián Rojas Rodríguez jhsrojasro@unal.edu.co |
+//|               Juan Andrés Gonzalez Arias jgonzalezar@unal.edu.co |
+//|                 Juan David Bello Rodrıguez jdbelloro@unal.edu.co |
+//|                   Simon Aparicio Bocanegra saparicio@unal.edu.co |
+//+------------------------------------------------------------------+
+package Frontera;
+public class MainFront {
+//+------------------------------------------------------------------+
+//| Main                                                             |
+//+------------------------------------------------------------------+
+/**
+ * @param args the command line arguments
+ */
+public static void main(String args[]) {
+    new Login().setVisible(true);
+}      
+}
